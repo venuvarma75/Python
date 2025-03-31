b709421262a6652d78f011d27c4c7d4068b5b2fe
venuvarma75 <venuvarma75@gmail.com>
2025-03-31 22:06:35 +0530

    Add files via upload

--- primenumbers .py
+++ primenumbers .py
@@ -1,3 +1,53 @@
+#5.A bank rewards its customers who have an account number that is a 'Perfect Number.'
+# A perfect number is a number whose sum of divisors 
+# (excluding itself) equals the number itself. Write a program to check whether
+# a given account number is a perfect number. 
+number=input("Enter a Account number:")
+number1=0
+sumofdivisors=0
+for i in range (1,number1):
+     if number % i == 0:
+         sumofdivisors+=1
+if sumofdivisors == 0:
+        print("Account number is perfect number")
+else:
+    print("Account number is not a  perfect number")
+ 
+
+
+#2. n=125 print all  the prime numbers in range  using nested loops
+
+num1 = 125
+for i in range (2,int(num1 ** 0.5) + 1):
+    if i % 2 == 0:
+        print(i)
+        spy=True
+        print("prime number")
+        break
+
+
+
+#5) A company is organizing a large-scale project and 
+# assigning teams sequential numbers (1, 2, 3, ... n). 
+# However, due to resource constraints, only even-numbered teams receive funding.
+n=20
+for i in range (1,n+1):
+     if i % 2 == 0:
+         print("funding")
+     else:
+         print("not funding")
+
+
+#9) Wap to print the sum of odd digits in a number. 
+# { input : 2355 output : 13}
+num1=15
+sum=2+3+5+5
+for i in range (1,sum):
+    if i % 2!= 0:
+        print(i)
+
+
+
 //method 1
 # num1=34
 # count=0
@@ -48,46 +98,3 @@
 #     else:
 #         print("not a prime number")            
             
-#5.A bank rewards its customers who have an account number that is a 'Perfect Number.'
-# A perfect number is a number whose sum of divisors 
-# (excluding itself) equals the number itself. Write a program to check whether
-# a given account number is a perfect number. 
-
-
-
-#2. n=125 print all  the prime numbers in range  using nested loops
-
-# num1 = 125
-
-
-# for i in range (2,int(num1 ** 0.5) + 1):
-#     if i % 2 == 0:
-#         print(i)
-#         spy=True
-#         print("prime number")
-#         break
-
-
-
-#5) A company is organizing a large-scale project and 
-# assigning teams sequential numbers (1, 2, 3, ... n). 
-# However, due to resource constraints, only even-numbered teams receive funding.
-#n=20
-# for i in range (1,n+1):
-#      if i % 2 == 0:
-#          print("funding")
-#      else:
-#          print("not funding")
-
-
-#9) Wap to print the sum of odd digits in a number. 
-# { input : 2355 output : 13}
-#num1=15
-# sum=2+3+5+5
-
-# for i in range (1,sum):
-#     if i % 2!= 0:
-#         print(i)
-
-
-
